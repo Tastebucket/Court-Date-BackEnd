3d3210e28aaf40218f32d1bbb8fb0273555d0dfb
Huy <h.qt193@gmail.com>
2023-02-16 18:09:31 -0500

    adding cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,10 @@
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name: "dcsbdscrq",
+    api_key: "239673437135639",
+    api_secret: "H7QWK8U4oC3pV6WECk3QLa1riyE"
+  })
+  
+
+module.export = cloudinary
